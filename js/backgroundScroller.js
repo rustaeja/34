9e617819cf3f3c57a75e234b3adb9eb97ff60793
kai91 <wngkai.91@gmail.com>
2014-01-25 06:42:50 -0800

    Added comments

--- js/backgroundScroller.js
+++ js/backgroundScroller.js
@@ -1,9 +1,11 @@
 
 var background1;
 var background2;
+var isTransitioning = false;
 
 var SeaBackground = enchant.Class.create(enchant.Sprite, {
 	initialize:function() {
+		// Load the sea image from asset
 		var game = enchant.Game.instance;
 		Sprite.call(this,800,600);
 		this.image = game.assets["assets/background/sea.jpg"];
@@ -11,7 +13,17 @@ var SeaBackground = enchant.Class.create(enchant.Sprite, {
 
 	onenterframe:function() {
 		this.x -= 2;
+		// If the background is completely at the left side of screen,
+		// move it to the right
 		if (this.x == -800) this.x = 800;
+
+		// If transitioning into the next stage, slowly sink the background
+		if (isTransitioning) this.y += 6;
+
+		// If the background is at bottom of the screen (invisible),
+		// remove it from the scene
+		if (this.y >= 600)
+			enchant.Game.instance.rootScene.removeChild(this);
 	}
 
 
@@ -19,6 +31,7 @@ var SeaBackground = enchant.Class.create(enchant.Sprite, {
 
 var SkyBackground = enchant.Class.create(enchant.Sprite, {
 	initialize:function() {
+		// load the sky image from background
 		var game = enchant.Game.instance;
 		Sprite.call(this, 800, 600);
 		this.image = game.assets["assets/background/sky.jpg"];
@@ -40,5 +53,5 @@ function getSeaBackground() {
 
 // Transition to the sky, to be implemented
 function toTheSky() {
-
+	isTransitioning = true;
 }
