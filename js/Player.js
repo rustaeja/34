a3b35ee99ea6e9629c2e16b597556ed930a8f850
kai91 <wngkai.91@gmail.com>
2014-01-25 08:18:37 -0800

    Added keyboard control to Player.js

--- js/Player.js
+++ js/Player.js
@@ -1,8 +1,23 @@
+var movementSpeed = 3.3;
 
 var Player = enchant.Class.create(enchant.Sprite, {
 	initialize: function() {
 		var game = enchant.Game.instance;
 		Sprite.call(this, 32, 32);
 		this.image = game.assets["assets/player/fish.png"];
+	},
+
+	onenterframe: function() {
+		var game = enchant.Game.instance;
+		if (game.input.left) {
+			this.x -= movementSpeed;
+			this.scaleX = -1;
+		}
+  	    if (game.input.right) {
+  	    	this.x += movementSpeed;
+  	    	this.scaleX = 1;
+  	    }
+  	    if (game.input.up) this.y -= movementSpeed;
+  	    if (game.input.down) this.y += movementSpeed;
 	}
 });
