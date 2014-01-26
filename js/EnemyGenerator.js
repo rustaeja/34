0c369544cdaac3918a0be4fb11ecc72bf37c7242
whiteSkar <sypower07@hotmail.com>
2014-01-26 02:23:15 -0800

    refactor some portion of code

--- js/EnemyGenerator.js
+++ js/EnemyGenerator.js
@@ -4,33 +4,35 @@
  
 var EnemyGenerator = enchant.Class.create(enchant.Node, {
 	initialize: function(enemiesMetaData, scene) {
+		var me = this;
+
 		this.enemiesMetaData = enemiesMetaData;
 		this.scene = scene;
 		this.activeEnemies = [];
         this.maxEnemies = 20;
+        this.replaceEnemyCallback = function(enemy) {
+	        	me.scene.removeChild(enemy);
+        		me.scene.addChild(me.replaceEnemy(enemy));
+        	};
 	},
 	genEnemy: function() {
 		var me = this,
 			randomIndex = Math.floor(Math.random() * this.enemiesMetaData.length),
 			gameInstance = enchant.Game.instance,
-			enemy = new Enemy(this.enemiesMetaData[randomIndex], this.scene, function(enemy) {
-	        	me.scene.removeChild(enemy);
-        		me.scene.addChild(me.replaceEnemy(enemy));
-        	});
+			enemy = new Enemy(this.enemiesMetaData[randomIndex], this.scene, this.replaceEnemyCallback);
 
-    		this.activeEnemies.push(enemy);
+		this.activeEnemies.push(enemy);
+
 		return enemy;
 	},
     replaceEnemy: function(enemy) {
     	var me = this,
     		randomIndex = Math.floor(Math.random() * this.enemiesMetaData.length),
         	index = this.activeEnemies.indexOf(enemy),
-        	newEnemy = new Enemy(this.enemiesMetaData[randomIndex], this.scene, function(enemy) {
-				me.scene.removeChild(enemy);
-				me.scene.addChild(me.replaceEnemy(enemy));
-			});
+        	newEnemy = new Enemy(this.enemiesMetaData[randomIndex], this.scene, this.replaceEnemyCallback);
 
-			this.activeEnemies.push(newEnemy);
+		this.activeEnemies.push(newEnemy);
+
         return newEnemy;
     },
 	moveEnemies: function(direction, movementSpeed) {
